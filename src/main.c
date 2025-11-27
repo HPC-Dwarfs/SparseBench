578c6c4096b1f5bc3a2b1c4df2ebc8719870323b
Jan Eitzinger <jan@moebiusband.org>
2025-11-27 22:23:33 +0100

    Separate command line argument handling

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
 #include <unistd.h>
 
 #include "allocate.h"
+#include "cli.h"
 #include "comm.h"
 #include "matrix.h"
 #include "matrixBinfile.h"
@@ -19,40 +20,8 @@
 #include "timing.h"
 #include "util.h"
 
-typedef enum { CG = 0, SPMV, GMRES, CHEBFD, NUMTYPES } types;
-
-#define HELPTEXT                                                               \
-  "Usage: sparseBench [options]\n\n"                                           \
-  "Options:\n"                                                                 \
-  "  -h         Show this help text\n"                                         \
-  "  -c <file name>   Convert MM matrix to binary matrix file.\n"              \
-  "  -f <parameter file>   Load options from a parameter file\n"               \
-  "  -m <MM matrix>   Load a matrix market file\n"                             \
-  "  -t <bench type>   Benchmark type, can be cg, spmv, or gmres. Default "    \
-  "cg.\n"                                                                      \
-  "  -x <int>   Size in x for generated matrix, ignored if MM file is "        \
-  "loaded. Default 100.\n"                                                     \
-  "  -y <int>   Size in y for generated matrix, ignored if MM file is "        \
-  "loaded. Default 100.\n"                                                     \
-  "  -z <int>   Size in z for generated matrix, ignored if MM file is "        \
-  "loaded. Default 100.\n"                                                     \
-  "  -i <int>   Number of solver iterations. Default 150.\n"                   \
-  "  -e <float>  Convergence criteria epsilon. Default 0.0.\n"
-
-#ifdef _MPI
-static void writeBinMatrix(Comm *c, char *filename) {
-  MMMatrix mm, mmLocal;
-  GMatrix m;
-  if (commIsMaster(c)) {
-    MMMatrixRead(&mm, filename);
-  }
-  commDistributeMatrix(c, &mm, &mmLocal);
-  matrixConvertfromMM(&mmLocal, &m);
-  matrixBinWrite(&m, c, changeFileEnding(filename, ".bmx"));
-}
-#endif
-
-static void initMatrix(Comm *c, Parameter *p, GMatrix *m) {
+static void initMatrix(Comm *c, Parameter *p, GMatrix *m)
+{
   if (strcmp(p->filename, "generate") == 0) {
     matrixGenerate(m, p, c->rank, c->size, false);
   } else if (strcmp(p->filename, "generate7P") == 0) {
@@ -60,7 +29,8 @@ static void initMatrix(Comm *c, Parameter *p, GMatrix *m) {
   } else {
     char *dot = strrchr(p->filename, '.');
     if (strcmp(dot, ".mtx") == 0) {
-      MMMatrix mm, mmLocal;
+      MMMatrix mm;
+      MMMatrix mmLocal;
 
       if (commIsMaster(c)) {
         printf("Read MTX matrix\n");
@@ -85,95 +55,19 @@ static void initMatrix(Comm *c, Parameter *p, GMatrix *m) {
   }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   Parameter param;
   Comm comm;
 
   commInit(&comm, argc, argv);
   initParameter(&param);
-
-  char *cvalue = NULL;
-  int index;
-  int type = CG;
-  bool stop = false;
-  int c;
-
-  opterr = 0;
-
-  while ((c = getopt(argc, argv, "hc:t:f:m:x:y:z:i:e:")) != -1)
-    switch (c) {
-    case 'h':
-      if (commIsMaster(&comm)) {
-        printf(HELPTEXT);
-      }
-      commAbort(&comm, "Finish write matrix");
-      break;
-    case 'c':
-#ifdef _MPI
-      writeBinMatrix(&comm, optarg);
-      commAbort(&comm, "Finish write matrix");
-#else
-      printf("Binary matrix files are only supported with MPI!\n");
-      exit(EXIT_SUCCESS);
-#endif
-    case 'f':
-      readParameter(&param, optarg);
-      break;
-    case 'm':
-      param.filename = optarg;
-      break;
-    case 't':
-      if (strcmp(optarg, "cg") == 0)
-        type = CG;
-      else if (strcmp(optarg, "spmv") == 0)
-        type = SPMV;
-      else if (strcmp(optarg, "gmres") == 0)
-        type = GMRES;
-      else if (strcmp(optarg, "cheb") == 0)
-        type = CHEBFD;
-      else {
-        printf("Unknown solver type %s\n", optarg);
-        return 1;
-      }
-      break;
-    case 'x':
-      param.nx = atoi(optarg);
-      break;
-    case 'y':
-      param.ny = atoi(optarg);
-      break;
-    case 'z':
-      param.nz = atoi(optarg);
-      break;
-    case 'i':
-      param.itermax = atoi(optarg);
-      break;
-    case 'e':
-      param.eps = atof(optarg);
-      break;
-    case '?':
-      if (optopt == 'c')
-        fprintf(stderr, "Option -%c requires an argument.\n", optopt);
-      else if (isprint(optopt))
-        fprintf(stderr, "Unknown option `-%c'.\n", optopt);
-      else
-        fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
-      return 1;
-    default:
-      abort();
-    }
-
-  for (index = optind; index < argc; index++) {
-    printf("Non-option argument %s\n", argv[index]);
-  }
-
-  if (stop) {
-    commAbort(&comm, "Wrong command line arguments");
-  }
-
+  parseArguments(&comm, &param, argc, argv);
   commPrintBanner(&comm);
 
-  double timeStart, timeStop, ts;
+  double timeStart;
+  double timeStop;
+  double ts;
   GMatrix m;
   timeStart = getTimeStamp();
   initMatrix(&comm, &param, &m);
@@ -191,8 +85,8 @@ int main(int argc, char **argv) {
   commBarrier();
   timeStop = getTimeStamp();
   if (commIsMaster(&comm)) {
-    printf("Parallel localization and matrix conversion took %.2fs\n",
-           timeStop - timeStart);
+    printf(
+        "Parallel localization and matrix conversion took %.2fs\n", timeStop - timeStart);
   }
 
   size_t factorFlops[NUMREGIONS];
@@ -203,13 +97,12 @@ int main(int argc, char **argv) {
   factorFlops[WAXPBY] = m.totalNr;
   factorWords[WAXPBY] = 3 * sizeof(CG_FLOAT) * m.totalNr;
   factorFlops[SPMVM]  = m.totalNnz;
-  factorWords[SPMVM]  = sizeof(CG_FLOAT) * m.totalNnz +
-                       sizeof(CG_UINT) * m.totalNnz;
+  factorWords[SPMVM]  = (sizeof(CG_FLOAT) * m.totalNnz) + (sizeof(CG_UINT) * m.totalNnz);
 
   profilerInit(factorFlops, factorWords);
 
   int k = 0;
-  switch (type) {
+  switch (BenchType) {
   case CG:
     if (commIsMaster(&comm)) {
       printf("Test type: CG\n");
@@ -221,10 +114,8 @@ int main(int argc, char **argv) {
       printf("Test type: SPMVM\n");
     }
     int itermax = param.itermax;
-    CG_FLOAT *x =
-        (CG_FLOAT *)allocate(ARRAY_ALIGNMENT, m.nc * sizeof(CG_FLOAT));
-    CG_FLOAT *y =
-        (CG_FLOAT *)allocate(ARRAY_ALIGNMENT, m.nr * sizeof(CG_FLOAT));
+    CG_FLOAT *x = (CG_FLOAT *)allocate(ARRAY_ALIGNMENT, m.nc * sizeof(CG_FLOAT));
+    CG_FLOAT *y = (CG_FLOAT *)allocate(ARRAY_ALIGNMENT, m.nr * sizeof(CG_FLOAT));
 
     for (int i = 0; i < m.nr; i++) {
       x[i] = (CG_FLOAT)1.0;
@@ -241,6 +132,7 @@ int main(int argc, char **argv) {
     }
 
     break;
+  default:
   }
 
   profilerPrint(&comm, k);
